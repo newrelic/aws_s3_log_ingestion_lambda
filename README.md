758bfc6b679a2a9af93ff481d20f5a2aa6710c57
William Hill <whill@newrelic.com>
2020-06-11 16:02:14 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,84 @@
+[![Community Project header](https://github.com/newrelic/open-source-office/raw/master/examples/categories/images/Community_Project.png)](https://github.com/newrelic/open-source-office/blob/master/examples/categories/index.md#community-project)
+
+
+
+# s3-log-ingestion-lambda [build badges go here when available]
+
+>AWS Serverless Application that sends log data from S3 to New Relic Logging.
+
+## Pre-requisites
+
+>To forward data to New Relic you'll need access to your license key. You can access your license key by following the steps listed here: [New Relic License Key](https://docs.newrelic.com/docs/accounts/install-new-relic/account-setup/license-key).
+
+>You will also need to configure your AWS credentials so that they are accessible from your local terminal: [Configuring the AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html)
+
+## Installation
+
+### Configure Lambda from AWS Severless Application Repository (SAR)
+
+> 1. Find "NewRelic-s3-log-ingestion" on the SAR (you have to check the "Show apps that create custom IAM roles or resource policies" box)
+> 2. Under "Application Settings" add your New Relic license key
+> 3. Acknowledge that this app creates custom IAM roles and press the "Deploy" button
+> 4. Wait for the "resources" to be created
+> 5. Navigate to AWS Lambda and open the functions
+> 6. Find "NewRelic-s3-log-ingestion" in the list and click on the function name
+> 7. Click on "Add Trigger" and selet the "S3" trigger
+> 8. Configure you "Bucket" with the bucket name of where you are logging to. 
+> 9. Configure "EventType" to "all object create events
+> 10. Optionally configure "prefix" and "suffix"
+> 11. Select "enable trigger" and click "Add"
+> 12. Congratulations your Lambda is successfully set up!
+> 13. To test your lambda setup:
+     1. Upload a new log file to your s3 bucket
+    2. Go to New Relic Logs and view your test logs
+
+
+### Configure Lambda Using Serverless
+
+Follow these steps below to deploy the log ingestion function manually. 
+
+> 1. Clone this repository: `git clone <REPO>`
+> 2. Install Serverless: `npm install -g serverless`
+> 3. Install the serverless-python-requirements plugin: `npm install`
+> 4. If not running Linux, [install Docker](https://docs.docker.com/install/) and keep it running
+> 5. [Retrieve your New Relic License Key](https://docs.newrelic.com/docs/accounts/install-new-relic/account-setup/license-key)
+> 6. Set the LICENSE_KEY environment variable: `export LICENSE_KEY=your-license-key-here`
+> 7. Set the SERVICE_NAME environment variable: `export SERVICE_NAME=your-service-name-here`
+> 8. Set the S3_BUCKET_NAME environment variable: `export S3_BUCKET_NAME=your-s3-bucket-name`
+> 9. [Optional] You can send logs from a specific location in the bucket only. Set the S3_Prefix (subdirectory name) `export S3_PREFIX=your-s3-subdirectory-name`
+> 10. Deploy the function: `serverless deploy`
+ 
+
+
+## Support
+
+New Relic hosts and moderates an online forum where customers can interact with New Relic employees as well as other customers to get help and share best practices. Like all official New Relic open source projects, there's a related Community topic in the New Relic Explorers Hub. You can find this project's topic/threads here:
+
+>Add the url for the support thread here
+
+## Contributing
+Full details about how to contribute to
+Contributions to improve s3-log-ingestion-lambda are encouraged! Keep in mind when you submit your pull request, you'll need to sign the CLA via the click-through using CLA-Assistant. You only have to sign the CLA one time per project.
+To execute our corporate CLA, which is required if your contribution is on behalf of a company, or if you have any questions, please drop us an email at opensource@newrelic.com.
+
+## License
+s3-log-ingestion-lambda is licensed under the [Apache 2.0](http://apache.org/licenses/LICENSE-2.0.txt) License.
+> The s3-log-ingestion-lambda also uses source code from third party libraries. Full details on which libraries are used and the terms under which they are licensed can be found in the third party notices document.
+
+## Troubleshooting FAQs
+
+```bash:
+ Error: docker: Cannot connect to the Docker daemon at unix:///var/run/docker.sock. Is the docker daemon running?.
+```
+This may mean either docker is not running or it has not been correctly set up. Please ensure docker is runnnig on your machine.
+
+
+.
+
+```bash:
+An error occurred when creating the trigger: Configurations overlap. Configurations on the same bucket cannot share a common event type.
+```
+If this error message appears while adding a Lambda "Trigger", ensure your s3 does not have another event of the same type on the bucket.
+Ie, your s3 bucket cannot have multiple "all object create events" events. 
+
+
---
+++ src/handler.py
@@ -0,0 +1,283 @@
+import json
+import urllib.parse
+import boto3
+import gzip
+import os
+import re
+from urllib import request
+import aiohttp
+import asyncio
+from smart_open import open
+
+from dotenv import load_dotenv
+load_dotenv(verbose=True)
+
+
+s3 = boto3.client('s3')
+
+US_LOGGING_INGEST_HOST = "https://log-api.newrelic.com/log/v1"
+EU_LOGGING_INGEST_HOST = 'https://log-api.eu.newrelic.com/log/v1'
+LOGGING_LAMBDA_VERSION = '1.0.0'
+LOGGING_PLUGIN_METADATA = {
+    'type': 's3-lambda',
+    'version': LOGGING_LAMBDA_VERSION
+}
+
+# Maximum number of retries
+MAX_RETRIES = 3
+# Initial backoff (in seconds) between retries
+INITIAL_BACKOFF = 1
+# Multiplier factor for the backoff between retries
+BACKOFF_MULTIPLIER = 2
+# Max length in bytes of the payload
+MAX_PAYLOAD_SIZE = 1000 * 1024
+
+
+class MaxRetriesException(Exception):
+    pass
+
+
+class BadRequestException(Exception):
+    pass
+
+
+def _get_license_key(license_key=None):
+    """
+    This functions gets New Relic's license key from env vars.
+    """
+    if license_key:
+        return license_key
+    return os.getenv("LICENSE_KEY", "")
+
+
+def _debug_logging_enabled():
+    """
+    Determines whether or not debug logging should be enabled based on the env var.
+    Defaults to false.
+    """
+    return os.getenv("DEBUG_ENABLED", "false").lower() == "true"
+
+
+async def http_post(session, url, data, headers):
+    def _format_error(e, text):
+        return "{}. {}".format(e, text)
+
+    backoff = INITIAL_BACKOFF
+    retries = 0
+
+    while retries < MAX_RETRIES:
+        if retries > 0:
+            print("Retrying in {} seconds".format(backoff))
+            await asyncio.sleep(backoff)
+            backoff *= BACKOFF_MULTIPLIER
+
+        retries += 1
+
+        try:
+            resp = await session.post(url, data=data, headers=headers)
+            resp.raise_for_status()
+            return resp.status, resp.url
+        except aiohttp.ClientResponseError as e:
+            if e.status == 400:
+                raise BadRequestException(
+                    _format_error(e, "Unexpected payload"))
+            elif e.status == 403:
+                raise BadRequestException(
+                    _format_error(e, "Review your license key"))
+            elif e.status == 404:
+                raise BadRequestException(
+                    _format_error(e, "Review the region endpoint")
+                )
+            elif e.status == 429:
+                print("There was a {} error. Reason: {}".format(
+                    e.status, e.message))
+                # Now retry the request
+                continue
+            elif 400 <= e.status < 500:
+                raise BadRequestException(e)
+
+    raise MaxRetriesException()
+
+####################
+
+
+def _get_logging_request_creator(payload, ingest_url=None, license_key=None):
+    def create_request():
+        req = request.Request(_get_logging_endpoint(ingest_url), payload)
+        req.add_header("X-License-Key", _get_license_key(license_key))
+        req.add_header("X-Event-Source", "logs")
+        req.add_header("Content-Encoding", "gzip")
+        return req
+
+    return create_request
+
+
+def _get_logging_endpoint(ingest_url=None):
+    """
+    Service url is determined by the license key's region.
+    Any other URL could be passed by using the NR_LOGGING_ENDPOINT env var.
+    """
+    if ingest_url:
+        return ingest_url
+    if "NR_LOGGING_ENDPOINT" in os.environ:
+        return os.environ["NR_LOGGING_ENDPOINT"]
+    return (
+        EU_LOGGING_INGEST_HOST
+        if _get_license_key().startswith("eu")
+        else US_LOGGING_INGEST_HOST
+    )
+
+
+def _split_log_payload(payload):
+    """
+    When data size is bigger than supported payload, it is divided in two
+    different requests
+    """
+    common = payload[0]["common"]
+    logs = payload[0]["logs"]
+    half = len(logs) // 2
+
+    return [
+        _reconstruct_log_payload(common, logs[:half]),
+        _reconstruct_log_payload(common, logs[half:]),
+    ]
+
+
+def _reconstruct_log_payload(common, logs):
+    return [{"common": common, "logs": logs}]
+
+
+async def _send_payload(request_creator, session, retry=False):
+    try:
+        req = request_creator()
+        status, url = await http_post(
+            session, req.get_full_url(), req.data, req.headers
+        )
+    except MaxRetriesException as e:
+        print("Retry limit reached. Failed to send log entry.")
+        if retry:
+            raise e
+    except BadRequestException as e:
+        print(e)
+    else:
+        print("Log entry sent. Response code: {}. url: {}".format(status, url))
+        return status
+
+
+def _generate_payloads(data, split_function):
+    """
+    Return a list of payloads to be sent to New Relic.
+    This method usually returns a list of one element, but can be bigger if the
+    payload size is too big
+    """
+    payload = gzip.compress(json.dumps(data).encode())
+
+    if len(payload) < MAX_PAYLOAD_SIZE:
+        return [payload]
+
+    split_data = split_function(data)
+    return _generate_payloads(split_data[0], split_function) + _generate_payloads(
+        split_data[1], split_function
+    )
+
+
+async def _send_log_entry(logLines, context, bucket_name):
+    """
+    This function sends the log entry to New Relic Logging's ingest
+    server. If it is necessary, entries will be split in different payloads
+    """
+    s3MetaData = {
+        "function_name": context.function_name,
+        "invoked_function_arn": context.invoked_function_arn,
+        "log_group_name": context.log_group_name,
+        "log_stream_name": context.log_stream_name,
+        "s3_bucket_name": bucket_name
+    }
+
+    async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=3)) as session:
+        requests = []
+        data = {"context": s3MetaData, "entry": logLines}
+
+        payload_data = _generate_payloads(
+            _package_log_payload(data), _split_log_payload)
+        for payload in payload_data:
+            requests.append(_send_payload(
+                _get_logging_request_creator(payload), session))
+
+        return await asyncio.gather(*requests)
+
+
+def _package_log_payload(data):
+    """
+    Packages up a MELT request for log messages
+    """
+    logLines = data["entry"]
+    log_messages = []
+
+    for line in logLines:
+        log_messages.append({'message': line})
+
+    packaged_payload = [
+        {
+            "common": {
+                "attributes": {
+                    "plugin": LOGGING_PLUGIN_METADATA,
+                    "aws": {
+                        "function_name": data["context"]["function_name"],
+                        "invoked_function_arn":data["context"]["invoked_function_arn"],
+                        "log_group_name": data["context"]["log_group_name"],
+                        "log_stream_name": data["context"]["log_stream_name"],
+                        "s3_bucket_name": data["context"]["s3_bucket_name"]},
+                }},
+            "logs": log_messages,
+        }]
+    return packaged_payload
+
+    def _split_log_payload(payload):
+        """
+        When data size is bigger than supported payload, it is divided in two
+        different requests
+        """
+        common = payload[0]["common"]
+        logs = payload[0]["logs"]
+        half = len(logs) // 2
+
+        return [
+            _reconstruct_log_payload(common, logs[:half]),
+            _reconstruct_log_payload(common, logs[half:]),
+        ]
+
+####################
+#  Lambda handler  #
+####################
+
+
+def lambda_handler(event, context):
+    # Get bucket from s3 upload event
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(
+        event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    try:
+        log_file_url = "s3://{}/{}".format(bucket, key)
+        logLines_object = []
+        for line in open(log_file_url, encoding='utf-8'):
+            logLines_object.append(json.loads(line))
+
+        asyncio.run(_send_log_entry(logLines_object, context, bucket))
+
+        return event
+    except KeyError as e:
+        print(e)
+        print(
+            'Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(
+                key,
+                bucket))
+        raise e
+    except OSError as e:
+        print(e)
+        print("Error processing the object {} from bucket {}.".format(key, bucket))
+        raise e
+
+
+if __name__ == "__main__":
+    lambda_handler('', '')
